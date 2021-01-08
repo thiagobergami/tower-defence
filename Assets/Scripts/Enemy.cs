47bdae0d37c57474d547c5c827fbfa5d54963a55
thiagobergami <thiagobergamig@gmail.com>
2021-01-07 21:57:31 -0300

    Create enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float _speed = 10f;
+
+    private Transform _target;
+    private int _wapointIndex = 0;
+
+    private void Start()
+    {
+        _target = Waypoints._wayPoints[0];
+    }
+
+    private void Update()
+    {
+        Vector3 movementDirection = _target.position - transform.position;
+        transform.Translate(movementDirection.normalized * _speed * Time.deltaTime, Space.World) ;
+
+        if(Vector3.Distance(transform.position, _target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    private void GetNextWayPoint()
+    {
+        if(_wapointIndex >= Waypoints._wayPoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        _wapointIndex++;
+        _target = Waypoints._wayPoints[_wapointIndex];
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] _wayPoints;
+
+    private void Awake()
+    {
+        _wayPoints = new Transform[transform.childCount];
+        for (int i = 0; i< _wayPoints.Length; i++)
+        {
+            _wayPoints[i] = transform.GetChild(i);
+        }
+    }
+}
